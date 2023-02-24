b98682e3bbf2590235659c6136f9f65afe58be4e
Rafael Santos <raffrenan@gmail.com>
2023-02-24 07:06:13 -0300

    Add auth context.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+import {onAuthStateChanged} from 'firebase/auth';
+import {useState, useEffect} from 'react';
+import {useAuthentication} from './hooks/useAuthentication';
+
+import {AuthProvider} from './context/AuthContext';
+
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Navbar from './components/Navbar/Navbar';
@@ -9,22 +16,38 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
 const App = () => {
-  return (
-    <div className='App'>
-      <BrowserRouter>
-          <Navbar />
-          <div className='container'>
-              <Routes>
-                  <Route path='/' element={<Home />} />
-                  <Route path='/about' element={<About />} />
-                  <Route path='/login' element={<Login />} />
-                  <Route path='/register' element={<Register />} />
-              </Routes>
-          </div>
-          <Footer />
-      </BrowserRouter>
-    </div>
-  );
-}
+
+    const [user, setUser] = useState(undefined);
+    const {auth} = useAuthentication();
+
+    const loadingUser = user === undefined;
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {setUser(user)})
+    }, [auth]);
+
+    if(loadingUser){
+        return <p>Carregando...</p>;
+    }
+
+    return (
+        <div className="App">
+            <AuthProvider value={user}>
+                <BrowserRouter>
+                    <Navbar/>
+                    <div className="container">
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/about" element={<About/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/register" element={<Register/>}/>
+                        </Routes>
+                    </div>
+                    <Footer/>
+                </BrowserRouter>
+            </AuthProvider>
+        </div>
+    );
+};
 
 export default App;
