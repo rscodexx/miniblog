a25249553d1906ce680b87fc8f8e32cf7d9489f0
Rafael Santos <raffrenan@gmail.com>
2023-02-23 23:41:22 -0300

    Configure react routes and initial routes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
-function App() {
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Mini Blog</h1>
+    <div className='App'>
+      <BrowserRouter>
+          <div className='container'>
+              <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/about' element={<About />} />
+              </Routes>
+          </div>
+      </BrowserRouter>
     </div>
   );
 }
