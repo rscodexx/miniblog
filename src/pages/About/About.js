f85a89a87801d592c1a0d5adab68f20091a365d7
Rafael Santos <raffrenan@gmail.com>
2023-02-24 07:31:01 -0300

    Finalize about page.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,12 @@
 import styles from './About.module.css';
+import {Link} from 'react-router-dom';
 
 const About = () => {
     return(
-        <div>
-            <h1>About</h1>
+        <div className={styles.about}>
+            <h2>Sobre o Mini <span>blog</span></h2>
+            <p>Esse projeto consiste em um blog feito com React no front-end e firebase no back-end.</p>
+            <Link to="/posts/create" className="btn">Criar Postagem</Link>
         </div>
     )
 }
