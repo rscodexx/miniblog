bc96d9f96714fb95cc23394ff137e962a7484ae2
Rafael Santos <raffrenan@gmail.com>
2023-03-04 15:49:55 -0300

    Finalize to view post page.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 const App = () => {
 
@@ -45,6 +46,7 @@ const App = () => {
                             <Route path="/" element={<Home/>}/>
                             <Route path="/about" element={<About/>}/>
                             <Route path="/search" element={<Search/>}/>
+                            <Route path="/posts/:id" element={<Post/>}/>
                             <Route path="/login" element={!user ? <Login /> : <Navigate to="/" /> }/>
                             <Route path="/register" element={!user ? <Register /> : <Navigate to="/register" /> }/>
                             <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/login" /> } />
